ba6e9c3843eb33f7e9bc3be23337817d8ff82730
Batuhan <batuhanyigitsengul@gmail.com>
2022-10-03 23:10:39 +0300

    %100

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import { GlobalStyle } from "./globalStyles";
 import { SliderData } from '../src/data/SlideData'
-import Dropdown from "./components/Mobile/Dropdown";
+import Dropdown from "./components/Mobile/index";
 import React, {useState} from "react";
 
 function App() {
